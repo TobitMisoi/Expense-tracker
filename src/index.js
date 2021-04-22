6b552bcba7d88b9691e9538046b411fe5243f647
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-22 16:21:15 +0300

    update path

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { SpeechProvider } from '@speechly/react-client'
-
 import App from './App'
-import { Provider } from './context/context'
+import { Provider } from './context'
+import './assets/styles/scss/main.scss'
 
-const appId = process.env.REACT_APP_APPID;
-
+const appId = process.env.REACT_APP_APPID
 
 ReactDOM.render(
   <SpeechProvider appId={appId} language='en-US'>
