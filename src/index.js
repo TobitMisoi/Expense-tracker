b9dba2546bb3dc1069c0260bc0d110f6e75e22d2
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-22 11:33:38 +0300

    hid token

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { SpeechProvider } from '@speechly/react-client';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { SpeechProvider } from '@speechly/react-client'
 
-import App from './App';
-import { Provider } from './context/context';
+import App from './App'
+import { Provider } from './context/context'
+
+const appId = process.env.REACT_APP_APPID;
+
 
 ReactDOM.render(
-    <SpeechProvider appId="4eee4bb2-7740-4419-bca6-1459765708b7" language="en-US">
-        <Provider>
-            <App />
-        </Provider>
-    </SpeechProvider>,
-    document.getElementById('root'));
-
+  <SpeechProvider appId={appId} language='en-US'>
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
+  document.getElementById('root')
+)
