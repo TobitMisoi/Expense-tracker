bd56102bfa754ac48c219712a5d731f85594f980
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-22 11:48:02 +0300

    prop types

--- src/components/Snackbar/Snackbar.jsx
+++ src/components/Snackbar/Snackbar.jsx
@@ -1,37 +1,46 @@
-import React from 'react';
-import { Snackbar } from '@material-ui/core';
-import { Alert as MuiAlert } from '@material-ui/lab';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Snackbar } from '@material-ui/core'
+import { Alert as MuiAlert } from '@material-ui/lab'
 
-import useStyles from './styles';
+import useStyles from './styles'
 
 const CustomizedSnackbar = ({ open, setOpen }) => {
+  const classes = useStyles()
 
-    const classes = useStyles();
+  const handleClose = (e, reason) => {
+    if (reason === 'clickaway') return
 
+    setOpen(false)
+  }
 
-    const handleClose = (e, reason) => {
-        if (reason === "clickaway") return;
-
-        setOpen(false);
-    }
-
-    return (
-        <div className={classes.root}>
-            <Snackbar
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                }}
-                open={open}
-                autoHideDuration={3000}
-                onClose={handleClose}
-            >
-                <MuiAlert onClose={handleClose} severity="success" elevation={6} variant="filled">
-                    Transaction succesfully created.
-                </MuiAlert>
-            </Snackbar>
-        </div>
-    )
+  return (
+    <div className={classes.root}>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+      >
+        <MuiAlert
+          onClose={handleClose}
+          severity='success'
+          elevation={6}
+          variant='filled'
+        >
+          Transaction succesfully created.
+        </MuiAlert>
+      </Snackbar>
+    </div>
+  )
 }
 
-export default CustomizedSnackbar;
+CustomizedSnackbar.propTypes = {
+  open: PropTypes.bool,
+  setOpen: PropTypes.bool
+}
+
+export default CustomizedSnackbar
--- src/context/context.js
+++ src/context/context.js
@@ -1,43 +1,51 @@
 import React, { createContext, useReducer } from 'react'
-import contextReducer from './ContextReducer';
+import contextReducer from './ContextReducer'
+import PropTypes from 'prop-types'
 
-const initialState = JSON.parse(localStorage.getItem('transactions')) || [];
+const initialState = JSON.parse(localStorage.getItem('transactions')) || []
 
-export const ExpenseTrackerContext = createContext(initialState);
+export const ExpenseTrackerContext = createContext(initialState)
 
 export const Provider = ({ children }) => {
+  const [transactions, dispatch] = useReducer(contextReducer, initialState)
 
-    const [transactions, dispatch] = useReducer(contextReducer, initialState)
+  console.log(transactions)
+  // Action Creators
+  const deleteTransaction = (id) => {
+    // dispatches something
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    })
+  }
 
-    console.log(transactions);
-    // Action Creators
-    const deleteTransaction = (id) => {
-        // dispatches something
-        dispatch({
-            type: 'DELETE_TRANSACTION',
-            payload: id
-        })
-    }
+  const addTransaction = (transaction) => {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    })
+  }
 
-    const addTransaction = (transaction) => {
-        dispatch({
-            type: 'ADD_TRANSACTION',
-            payload: transaction
-        })
-    }
+  const balance = transactions.reduce((acc, currVal) => {
+    return currVal.type === 'Expense'
+      ? acc - currVal.amount
+      : acc + currVal.amount
+  }, 0)
 
-    const balance = transactions.reduce((acc, currVal) => {
-        return (currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount)
-    }, 0)
+  return (
+    <ExpenseTrackerContext.Provider
+      value={{
+        addTransaction,
+        deleteTransaction,
+        transactions,
+        balance
+      }}
+    >
+      {children}
+    </ExpenseTrackerContext.Provider>
+  )
+}
 
-    return (
-        <ExpenseTrackerContext.Provider value={{
-            addTransaction,
-            deleteTransaction,
-            transactions,
-            balance
-        }}>
-            {children}
-        </ExpenseTrackerContext.Provider>
-    )
+Provider.propTypes = {
+  children: PropTypes.element
 }
