05fe4c7c8f3c3713eff9310ba42706c1e71eb926
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-23 11:44:36 +0300

    lets go

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react'
-// import { Grid } from '@material-ui/core'
-
-// import Details from './components/Details/Details'
-// import Main from './components/Main/Main'
 import {
   ErrorPanel,
   PushToTalkButton,
   PushToTalkButtonContainer
 } from '@speechly/react-ui'
-// import useStyles from './styles'
 import { Dashboard } from './views'
 
 const App = () => {
-  //   const classes = useStyles()
-
   return (
     <>
       <div className='content'>
