1ba8b39a2da1f3c965f17f44ac3bb1c6ca0d740e
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-23 10:59:38 +0300

    lint add export

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,37 +1,63 @@
-import React, { useContext } from 'react';
-import { Avatar, IconButton, List as MUIList, ListItem, ListItemAvatar, ListItemSecondaryAction, ListItemText, Slide } from '@material-ui/core';
-import useStyles from './styles';
-import { Delete, MoneyOff } from '@material-ui/icons';
-import { ExpenseTrackerContext } from '../../../context/context';
+import React, { useContext } from 'react'
+import {
+  Avatar,
+  IconButton,
+  List as MUIList,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  Slide
+} from '@material-ui/core'
+import useStyles from './styles'
+import { Delete, MoneyOff } from '@material-ui/icons'
+import { ExpenseTrackerContext } from '../../../context/context'
 
 const List = () => {
-    const classes = useStyles();
+  const classes = useStyles()
 
-    const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext);
+  const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext)
 
-    return (
-        <MUIList dense={false} className={classes.list}>
-            {
-                transactions.map((transaction) => (
-                    <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
-                        <ListItem>
-                            <ListItemAvatar>
-                                <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
-                                    <MoneyOff />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
-                            <ListItemSecondaryAction>
-                                <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
-                                    <Delete />
-                                </IconButton>
-                            </ListItemSecondaryAction>
-                        </ListItem>
-                    </Slide>
-                ))
-            }
-        </MUIList>
-    )
+  return (
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map((transaction) => (
+        <Slide
+          direction='down'
+          in
+          mountOnEnter
+          unmountOnExit
+          key={transaction.id}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar
+                className={
+                  transaction.type === 'Income'
+                    ? classes.avatarIncome
+                    : classes.avatarExpense
+                }
+              >
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={transaction.category}
+              secondary={`$${transaction.amount} - ${transaction.date}`}
+            />
+            <ListItemSecondaryAction>
+              <IconButton
+                edge='end'
+                aria-label='delete'
+                onClick={() => deleteTransaction(transaction.id)}
+              >
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
+  )
 }
 
-export default List;
+export default List
