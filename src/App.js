68fd68edc3058a8374f9134530d407704a5cf89a
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-23 11:38:32 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import { Grid } from '@material-ui/core';
-import React from 'react';
-import Details from './components/Details/Details';
-import Main from './components/Main/Main';
-import './index.css';
-import { ErrorPanel, PushToTalkButton, PushToTalkButtonContainer } from '@speechly/react-ui';
-import useStyles from './styles';
+import React from 'react'
+// import { Grid } from '@material-ui/core'
+
+// import Details from './components/Details/Details'
+// import Main from './components/Main/Main'
+import {
+  ErrorPanel,
+  PushToTalkButton,
+  PushToTalkButtonContainer
+} from '@speechly/react-ui'
+// import useStyles from './styles'
+import { Dashboard } from './views'
 
 const App = () => {
-    const classes = useStyles();
+  //   const classes = useStyles()
 
-
-    return (
-        <div>
-            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }} >
-                <Grid item xs={12} sm={4} className={classes.mobile} >
-                    <Details title="Income" />
-                </Grid>
-                <Grid item xs={12} sm={3} className={classes.main} >
-                    <Main />
-                </Grid>
-                <Grid item xs={12} sm={4} className={classes.desktop} >
-                    <Details title="Income" />
-                </Grid>
-                <Grid item xs={12} sm={4} className={classes.last} >
-                    <Details title="Expense" />
-                </Grid>
-            </Grid>
-            <PushToTalkButtonContainer>
-                <PushToTalkButton />
-                <ErrorPanel />
-            </PushToTalkButtonContainer>
+  return (
+    <>
+      <div className='content'>
+        <div className='container'>
+          <Dashboard />
         </div>
-    )
+      </div>
+      <PushToTalkButtonContainer>
+        <PushToTalkButton />
+        <ErrorPanel />
+      </PushToTalkButtonContainer>
+    </>
+  )
 }
 
-export default App;
+export default App
