4e50a2ebccb9ac177d273d286fd1fef860c0bc93
Tobit Misoi <misoitobit@zetech.ac.ke>
2021-04-23 11:30:09 +0300

    rm console

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -1,33 +1,40 @@
-import { useContext } from 'react';
-import { expenseCategories, incomeCategories, resetCategories } from './constants/categories';
-import { ExpenseTrackerContext } from './context/context';
+import { useContext } from 'react'
+import {
+  expenseCategories,
+  incomeCategories,
+  resetCategories
+} from './constants/categories'
+import { ExpenseTrackerContext } from './context/context'
 
 const useTransactions = (title) => {
-    resetCategories();
-    const { transactions } = useContext(ExpenseTrackerContext);
+  resetCategories()
+  const { transactions } = useContext(ExpenseTrackerContext)
 
-    const transactionsPerType = transactions.filter((t) => t.type === title)
-    const total = transactionsPerType.reduce((acc, currVal) => acc += currVal.amount, 0);
-    const categories = title === 'Income' ? incomeCategories : expenseCategories;
+  const transactionsPerType = transactions.filter((t) => t.type === title)
+  const total = transactionsPerType.reduce(
+    (acc, currVal) => (acc += currVal.amount),
+    0
+  )
+  const categories = title === 'Income' ? incomeCategories : expenseCategories
 
-    console.log("See this:", { total, transactionsPerType, categories });
+  transactionsPerType.forEach((t) => {
+    const category = categories.find((c) => c.type === t.category)
 
-    transactionsPerType.forEach(t => {
-        const category = categories.find((c) => c.type === t.category);
+    if (category) category.amount += t.amount
+  })
+  const filteredCategories = categories.filter((sc) => sc.amount > 0)
 
-        if (category) category.amount += t.amount;
-    });
-    const filteredCategories = categories.filter((sc) => sc.amount > 0);
+  const chartData = {
+    datasets: [
+      {
+        data: filteredCategories.map((c) => c.amount),
+        backgroundColor: filteredCategories.map((c) => c.color)
+      }
+    ],
+    labels: filteredCategories.map((c) => c.type)
+  }
 
-    const chartData = {
-        datasets: [{
-            data: filteredCategories.map((c) => c.amount),
-            backgroundColor: filteredCategories.map((c) => c.color),
-        }],
-        labels: filteredCategories.map((c) => c.type),
-    };
-
-    return { total, chartData }
+  return { total, chartData }
 }
 
-export default useTransactions;
+export default useTransactions
